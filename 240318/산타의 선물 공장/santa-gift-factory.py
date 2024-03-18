e9e270fc273f9f2a217ea0e863c6bb88b5cece54
Seungheon <101968287+Githarold@users.noreply.github.com>
2024-03-18 11:56:51 +0900

    Title: 산타의 선물 공장, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240318/산타의 선물 공장/santa-gift-factory.py
+++ 240318/산타의 선물 공장/santa-gift-factory.py
@@ -6,51 +6,63 @@ q = int(input().strip()) - 1
 tmp = list(map(int, input().split()))
 n, m = tmp[1:3]
 belt_info = {i: True for i in range(m)}
+box_info = {}
 
 belts = [deque() for _ in range(m)]
 for i in range(n):
     belt_num = i // (n//m)
-    belts[belt_num].append((tmp[i+3], tmp[i+n+3]))
+    box_id, box_weight = tmp[i+3], tmp[i+n+3]
+    belts[belt_num].append((box_id, box_weight))
+    box_info[box_id] = belt_num
 
 def work1(w_max):
-    global belts
+    global belts, box_info
     weight_sum = 0
     for i, belt in enumerate(belts):
         if belt_info[i] and belt:
             if belt[0][1] <= w_max:
                 weight_sum += belt[0][1]
-                belt.popleft()
+                tmp = belt.popleft()
+                del box_info[tmp[0]]
             else:
                 belt.append(belt.popleft())
+
     print(weight_sum)
 
 def work2(r_id):
-    global belts
-    for i, belt in enumerate(belts):
-        if belt_info[i] and belt:
-            for b in list(belt):
+    global belts, box_info
+    belt_num = box_info.get(r_id, None)
+    if belt_num is None:
+        print(-1)
+    else:
+        curr_belt = belts[belt_num]
+        if belt_info[belt_num] and curr_belt:
+            for b in list(curr_belt):
                 if b[0] == r_id:
-                    belt.remove(b)
+                    curr_belt.remove(b)
+                    del box_info[b[0]]
                     print(r_id)
-                    return
-    print(-1)
+                    return        
 
 def work3(f_id):
     global belts
-    for i, belt in enumerate(belts):
-        if belt_info[i] and belt:
-            belt_list = list(belt)
+    belt_num = box_info.get(f_id, None)
+    if belt_num is None:
+        print(-1)
+    else:
+        curr_belt = belts[belt_num]
+        if belt_info[belt_num] and curr_belt:
+            belt_list = list(curr_belt)
             belt_len = len(belt_list)
             for j, b in enumerate(belt_list):
                 if b[0] == f_id:
                     for _ in range(j, belt_len):
-                        belt.appendleft(belt.pop())
+                        curr_belt.appendleft(belt.pop())
                     print(i+1)
                     return
-    print(-1)
 
 def work4(b_num):
-    global belts, belt_info
+    global belts, belt_info, box_info
     remove_belt = b_num-1
     if belt_info[remove_belt]:
         belt_info[remove_belt] = False
@@ -58,7 +70,9 @@ def work4(b_num):
         while not belt_info[add_belt]:
             add_belt = (add_belt + 1) % m
         while belts[remove_belt]:
-            belts[add_belt].append(belts[remove_belt].popleft())
+            append_item = belts[remove_belt].popleft()
+            box_info[append_item[0]] = add_belt
+            belts[add_belt].append(append_item)
         print(b_num)
     else:
         print(-1)
