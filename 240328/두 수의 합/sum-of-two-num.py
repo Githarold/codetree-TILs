9ac37559d6982440d0521c310d7b282c34cbb0e8
Seungheon <101968287+Githarold@users.noreply.github.com>
2024-03-28 10:26:00 +0900

    Title: 두 수의 합, Time: 239ms, Memory: 36MB, Status: Passed - CodeTree

--- 240328/두 수의 합/sum-of-two-num.py
+++ 240328/두 수의 합/sum-of-two-num.py
@@ -1,13 +1,19 @@
-import sys
-input = sys.stdin.readline
-
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 dic = {}
-for i in range(n):
-    for j in range(i+1, n):
-        sum = arr[i] + arr[j]
-        dic[sum] = dic.get(sum, 0) + 1
+for num in arr:
+    dic[num] = dic.get(num, 0) + 1
 
-print(dic[k])
+count = 0
+for num in arr:
+    target = k - num
+    # 현재 수를 딕셔너리에서 제거하거나 감소시켜 중복 계산 방지
+    dic[num] -= 1
+    if target in dic and dic[target] > 0:
+        count += dic[target]
+    # 해당 수가 더 이상 없다면, 딕셔너리에서 제거
+    if dic[num] == 0:
+        del dic[num]
+
+print(count)
