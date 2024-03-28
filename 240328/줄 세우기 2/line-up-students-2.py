58b324ac39e21070bef0c65744928255ee7c8746
Seungheon <101968287+Githarold@users.noreply.github.com>
2024-03-28 11:53:14 +0900

    Title: 줄 세우기 2, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240328/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+students = []
+
+for i in range(n):
+    h, w = map(int, input().split())
+    students.append((h, w, i + 1))
+
+students.sort(key=lambda x: (x[0], -x[1], x[2]))
+
+for student in students:
+    print(student[0], student[1], student[2])
