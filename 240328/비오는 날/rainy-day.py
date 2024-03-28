9ffa3d4c1f61b6180d75afffa645e11c9b0c54da
Seungheon <101968287+Githarold@users.noreply.github.com>
2024-03-28 11:42:26 +0900

    Title: 비오는 날, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240328/비오는 날/rainy-day.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+def early(day1, day2):
+    date1, _ = day1.split()
+    date2, _ = day2.split()
+    y1, m1, d1 = map(int, date1.split('-'))
+    y2, m2, d2 = map(int, date2.split('-'))
+
+    flag = True
+    if y1 < y2:
+        flag = False
+    elif y1 == y2:
+        if m1 < m2:
+            flag = False
+        elif m1 == m2:
+            if d1 < d2:
+                flag = False
+
+    if flag:
+        return day2
+    else:
+        return day2
+
+rainy = None
+n = int(input().strip())
+for _ in range(n):
+    date, weekday, wether = input().split()
+    if wether == "Rain":
+        tmp_day = date + ' ' + weekday
+        if not rainy:
+            rainy = tmp_day
+        else:
+            rainy = early(tmp_day, rainy)
+
+print(rainy, 'Rain')
