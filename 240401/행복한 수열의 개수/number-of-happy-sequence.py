14d43b2af7a3ed5b2cd81f1ddd48670f59e41e3b
Seungheon <101968287+Githarold@users.noreply.github.com>
2024-04-01 20:07:08 +0900

    Title: 행복한 수열의 개수, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240401/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240401/행복한 수열의 개수/number-of-happy-sequence.py
@@ -8,30 +8,16 @@ for _ in range(n):
     board.append(list(map(int, input().split())))
 
 answer = 0
-for i in range(n):
-    last_num1 = None
-    last_num2 = None
-    cnt1 = 1
-    cnt2 = 1
-    for j in range(n):
-        if last_num1 and board[i][j] == last_num1:
-            cnt1 += 1
-        else:
-            cnt1 = 1
-            last_num1 = board[i][j]
-
-        if cnt1 >= m:
+for row in board:
+    for i in range(n-m+1):
+        if row[i:i+m].count(row[i]) == m:
             answer += 1
-            cnt1 = -100
+            break
 
-        if last_num2 and board[j][i] == last_num2:
-            cnt2 += 1
-        else:
-            cnt2 = 1
-            last_num2 = board[j][i]
-
-        if cnt2 >= m:
+for cow in zip(*board):
+    for i in range(n-m+1):
+        if cow[i:i+m].count(cow[i]) == m:
             answer += 1
-            cnt2 = -100
+            break
 
 print(answer)
